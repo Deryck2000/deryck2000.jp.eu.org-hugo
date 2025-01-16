7c6d371b60de38cce7439efd89314a7f9c281b49
Deryck2000 <90670700+Deryck2000@users.noreply.github.com>
2025-01-16 23:09:32 +0900

    テスト

--- doka.bat
+++ doka.bat
@@ -10,17 +10,25 @@ set "repo1_path=..\deryck2000.jp.eu.org-hugo"
 rem 二つ目のリポジトリのパス
 set "repo2_path=..\deryck2000.jp.eu.org"
 
-rem 一つ目のリポジトリに移動してコミット・プッシュ
+if "%commit_message%"=="" (
+    echo コミットメッセージを引数として指定してください
+    exit /b 1
+)
+
+rem 一つ目のリポジトリに移動してフェッチ・プル・コミット・プッシュ
 cd /d "%repo1_path%"
+git fetch
+git pull
 git add .
 git commit -m "%commit_message%"
 git push origin main
 
-rem 二つ目のリポジトリに移動してコミット・プッシュ
+rem 二つ目のリポジトリに移動してフェッチ・プル・コミット・プッシュ
 cd /d "%repo2_path%"
+git fetch
+git pull
 git add .
 git commit -m "%commit_message%"
 git push origin main
 
 endlocal
-pause
